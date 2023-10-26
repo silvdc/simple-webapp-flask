83a731357667b9a8f69948ef3db1c40bff39d2c6
silvdc <silvdc@gmail.com>
2023-10-26 14:59:35 -0700

    Update app.py
    Add health check

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/health')
+def health():
+    return 'OK'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
